f323c42854af09f944fb53aa71ba7f5e52bddc18
hetvihradadiya <hetvi@techeniac.com>
2026-06-15 17:53:53 +0530

    Challenge-#1

--- 01-Fundamentals-Part-1/starter/Challenge-1.js
+++ 01-Fundamentals-Part-1/starter/Challenge-1.js
@@ -7,4 +7,5 @@ mass_j = 92;
 BMI_m = mass_m / (height_m * height_m);
 BMI_j = mass_j / (height_j * height_j);
 
-markHigherBMI = console.log(max(BMI_j, BMI_m));
+markHigherBMI = BMI_j > BMI_m;
+console.log(markHigherBMI);

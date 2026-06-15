f7024bf4128ca82697c42ed5ef8b6e03438ddfa9
hetvihradadiya <hetvi@techeniac.com>
2026-06-15 17:48:55 +0530

    Challenge-#1

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,2 +1,35 @@
 let js = "Amazing";
-if (js === "Amazing") alert("Javascript is FUN!!");
+// if (js === "Amazing") alert("Javascript is FUN!!");
+
+console.log(40 - 80 + 100 * 10);
+
+console.log(23.1);
+
+firstName = "Hetvi";
+lastName = "Radadiya";
+
+console.log(firstName + " " + lastName);
+
+// Challenge 1
+// height_m = 1.69;
+// mass_m = 78;
+
+// height_j = 1.95;
+// mass_j = 92;
+
+height_m = 1.88;
+mass_m = 95;
+
+height_j = 1.76;
+mass_j = 85;
+
+BMI_m = mass_m / (height_m * height_m);
+BMI_j = mass_j / (height_j * height_j);
+
+if (BMI_j > BMI_m) {
+  markHigherBMI = "John";
+} else {
+  markHigherBMI = "Mark";
+}
+
+console.log(markHigherBMI);

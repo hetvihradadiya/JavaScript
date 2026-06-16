2bf87fd109cf690f86c2e35912001881c985f152
hetvihradadiya <hetvi@techeniac.com>
2026-06-16 11:10:51 +0530

    functions basics and arrow functions

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -33,3 +33,71 @@ if (BMI_j > BMI_m) {
 }
 
 console.log(markHigherBMI);
+
+/* 
+
+completed
+
+10. Practice Assignments
+
+11. Data Types
+
+
+12. let, const and var
+
+
+13. Basic Operators
+
+
+14. Operator Precedence
+
+17. Strings and Template Literals
+
+
+18. Taking Decisions: if / else Statements
+
+19. CHALLENGE #2: Video Solution
+
+20. Type Conversion and Coercion
+
+
+21. Truthy and Falsy Values
+
+
+22. Equality Operators: == vs. ===
+
+
+23. Boolean Logic
+
+24. Logical Operators
+
+26. The switch Statement
+
+
+27. Statements and Expressions
+
+28. The Conditional (Ternary) Operator
+
+30. JavaScript Releases: ES5, ES6+ and ESNext
+14min
+*/
+
+const calcAge1 = function (birthyear) {
+  return 2027 - birthyear;
+};
+
+birthyear = 2006;
+
+console.log(calcAge1(birthyear));
+
+const calcAge3 = (bYear) => 2027 - bYear;
+
+console.log(calcAge1(birthyear));
+
+const yearsOfRetirement = (birthYear, firstName) => {
+  const age = 2026 - birthYear;
+  const retire = 65 - age;
+  return `${firstName} will retire in ${retire} years.`;
+};
+
+console.log(yearsOfRetirement(2005, "Hetvi"));
